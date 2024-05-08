81039e5ef43ecf35b44302f3208705adad45d1b0
Rami Saarivuori <37345715+Appelsiini1@users.noreply.github.com>
2024-05-08 17:10:10 +0300

    Init React in app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ButtonComp from "./components/Button";
+
+function App() {
+  return (
+    <div>
+      <h1>💖 Hello World!</h1>
+      <p>Welcome to your Electron application.</p>
+      <ButtonComp />
+    </div>
+  );
+}
+
+export default App;
