b3d7fa0d83bc25f128e5d7cae8e011d498b1c5e3
Rami Saarivuori <167883444+RSaarivuoriLUT@users.noreply.github.com>
2024-05-27 12:22:03 +0300

    Add confirmation modal window

---
+++ src/components/ModalConfirm.tsx
@@ -0,0 +1,64 @@
+import Button from "@mui/joy/Button";
+import Divider from "@mui/joy/Divider";
+import DialogTitle from "@mui/joy/DialogTitle";
+import DialogContent from "@mui/joy/DialogContent";
+import DialogActions from "@mui/joy/DialogActions";
+import Modal from "@mui/joy/Modal";
+import ModalDialog from "@mui/joy/ModalDialog";
+import ThumbsUpDownIcon from "@mui/icons-material/ThumbsUpDown";
+import { useState } from "react";
+import {
+  ui_ok,
+  ui_confirm,
+  ui_cancel,
+  ui_confirm_header,
+} from "../../resource/texts.json";
+
+export default function ModalConfirm({
+  language,
+}: {
+  language: keyof typeof ui_ok;
+}) {
+  const [open, setOpen] = useState<boolean>(false);
+  return (
+    <>
+      <Button
+        variant="solid"
+        sx={{
+          color: "#00000",
+          backgroundColor: "#F8A866",
+          "&:hover": { backgroundColor: "#F68C35" },
+        }}
+        onClick={() => setOpen(true)}
+      >
+        {ui_ok[language]}
+      </Button>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <ModalDialog variant="outlined" role="alertdialog">
+          <DialogTitle>
+            <ThumbsUpDownIcon />
+            {ui_confirm_header[language]}
+          </DialogTitle>
+          <Divider />
+          <DialogContent>{ui_confirm[language]}</DialogContent>
+          <DialogActions>
+            <Button
+              variant="solid"
+              color="success"
+              onClick={() => setOpen(false)}
+            >
+              {ui_ok[language]}
+            </Button>
+            <Button
+              variant="plain"
+              color="neutral"
+              onClick={() => setOpen(false)}
+            >
+              {ui_cancel[language]}
+            </Button>
+          </DialogActions>
+        </ModalDialog>
+      </Modal>
+    </>
+  );
+}
