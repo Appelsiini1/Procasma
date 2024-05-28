90424c1d5c0cf4f7a9a5073834592d3e54bbe6ff
Rami Saarivuori <37345715+Appelsiini1@users.noreply.github.com>
2024-05-28 15:52:22 +0300

    Add currentCourse to UI constants

--- src/types.ts
+++ src/types.ts
@@ -1,48 +1,68 @@
-export type SupportedLanguages = "FI" | "ENG"
+export type SupportedLanguages = "FI" | "ENG";
 
-export type FileTypes = "text" | "image" | "code"
+export type FileTypes = "text" | "image" | "code";
 
 export interface FileData {
-  "fileName": string,
-  "path": string,
-  "solution": boolean,
-  "resultFile": boolean,
-  "show_student": boolean,
-  "fileType": FileTypes
-  }
+  fileName: string;
+  path: string;
+  solution: boolean;
+  resultFile: boolean;
+  show_student: boolean;
+  fileType: FileTypes;
+}
 
 export interface CGData {
-  "id": string,
-  "atv2": object
+  id: string;
+  atv2: object;
 }
 
 export interface AssignmentData {
-  "assignmentID": string,
-  "assignmentType": "assignment",
-  "assignment": {
-      "title": string,
-      "tags": Array<string>,
-      "module": number | null,
-      "assignmentNo": Array<number>,
-      "level": number | null,
-      "next": Array<string> | null,
-      "previous": Array<string> | null,
-      "codeLanguage": string,
-      "variations": {
-        [key:string]: {
-          "instructions": string,
-          "exampleRuns": {
-            [key:string]: {
-              "generate": boolean,
-              "inputs": Array<string | number>,
-              "cmdInputs": Array<string | number>,
-              "output": string,
-              },
-            },
-          "files": Array<FileData>
-          "usedIn": Array<string>,
-          "cgConfig": CGData
-      },
-    },
-  }
+  assignmentID: string;
+  assignmentType: "assignment";
+  assignment: {
+    title: string;
+    tags: Array<string>;
+    module: number | null;
+    assignmentNo: Array<number>;
+    level: number | null;
+    next: Array<string> | null;
+    previous: Array<string> | null;
+    codeLanguage: string;
+    variations: {
+      [key: string]: {
+        instructions: string;
+        exampleRuns: {
+          [key: string]: {
+            generate: boolean;
+            inputs: Array<string | number>;
+            cmdInputs: Array<string | number>;
+            output: string;
+          };
+        };
+        files: Array<FileData>;
+        usedIn: Array<string>;
+        cgConfig: CGData;
+      };
+    };
+  };
 }
+
+export interface CourseData {
+  title?: string;
+  ID?: string;
+  modules?: number;
+  moduleType?: "week" | "module" | null;
+  language?: SupportedLanguages;
+  codeLanguage?: string | null;
+  CodeGradeID?: number;
+  minLevel?: number;
+  maxLevel?: number;
+  levels?: {
+    [key: number]: {
+      fullName: string;
+      abbreviation: string;
+    };
+  };
+}
+
+export type CourseLoaderData = "create" | "manage";
