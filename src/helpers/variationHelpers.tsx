30e4d740156440cd6218dbfc77bab905f952b05f
Rami Saarivuori <167883444+RSaarivuoriLUT@users.noreply.github.com>
2024-06-04 16:26:51 +0300

    Change variation helpers to functions

--- src/helpers/variationHelpers.tsx
+++ src/helpers/variationHelpers.tsx
@@ -1,47 +1,29 @@
 import React from "react";
 import VariationComponent from "../components/VariationComponent";
 
-export class VariationAccordion {
-  private _variationAccordion: Array<React.JSX.Element> = null;
-  private _setAccordion: React.Dispatch<
-    React.SetStateAction<React.JSX.Element[]>
-  > = null;
-  private _pageType: unknown = null;
-
-  constructor(
-    accordion: Array<React.JSX.Element>,
-    setAccordion: React.Dispatch<React.SetStateAction<React.JSX.Element[]>>,
-    pageType: unknown
-  ) {
-    this._variationAccordion = accordion;
-    this._setAccordion = setAccordion;
-    this._pageType = pageType;
-    // add existing vars
+export function deleteVariation(
+  variations: Array<React.JSX.Element>,
+  setVariations: React.Dispatch<React.SetStateAction<React.JSX.Element[]>>,
+  varID: string,
+  pageType: string
+) {
+  if (pageType !== "new") {
+    // call some function through IPC that deletes it from disk
+  } else {
+    setVariations(
+      variations.filter((element) => (element.key === varID ? false : true))
+    );
   }
+}
 
-  deleteVariation(varID: string) {
-    if (this._pageType !== "new") {
-      // call some function through IPC that deletes it from disk
-    } else {
-      this._setAccordion(
-        this._variationAccordion.filter((element) =>
-          element.key === varID ? false : true
-        )
-      );
-    }
-  }
-
-  addVariation() {
-    if (!this._variationAccordion) {
-      this._setAccordion([
-        <VariationComponent varID="A" key="A" self={this} />,
-      ]);
-    } else {
-      // function to get next available varID
-      this._setAccordion([
-        ...this._variationAccordion,
-        <VariationComponent varID="B" key="B" self={this} />,
-      ]);
-    }
+export function addVariation(
+  variations: Array<React.JSX.Element> | null,
+  setVariations: React.Dispatch<React.SetStateAction<React.JSX.Element[]>>
+) {
+  if (!variations) {
+    setVariations([<VariationComponent varID="A" key="A" />]);
+  } else {
+    // function to get next available varID
+    setVariations([...variations, <VariationComponent varID="B" key="B" />]);
   }
 }
