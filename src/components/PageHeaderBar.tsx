17a65ec9fd1e435aaf19f73394fef9ebf19615f3
Rami Saarivuori <167883444+RSaarivuoriLUT@users.noreply.github.com>
2024-06-14 16:12:19 +0300

    Make header sticky

--- src/components/PageHeaderBar.tsx
+++ src/components/PageHeaderBar.tsx
@@ -29,6 +29,8 @@ export default function PageHeaderBar({ pageName }: ButtonProps) {
         backgroundColor: "#F8A866",
         minHeight: "3.5rem",
         boxShadow: "0px 1px 3px 3px rgb(0 0 0 / 20%)",
+        position: "sticky",
+        top: "0",
       }}
     >
       <Grid
