dc960e769feca51e59d39cb338684a50303a008c
eliassyyrilaLUT <elias.syyrila@lut.fi>
2024-07-12 14:23:47 +0300

    getAll db functions added

--- src/helpers/testDatabase.ts
+++ src/helpers/testDatabase.ts
@@ -1,22 +1,12 @@
 import * as DatabaseFunc from "../helpers/databaseOperations";
 import * as testGlobals from "../myTestGlobals";
 
-export async function testDatabase() {
-  //const testPath = "C:\\Users\\ramis\\Documents\\Procasma database test";
-  const testPath = "C:\\Users\\Elias\\Documents\\DEV\\Procasma\\" 
-  //   const dbPromise = new Promise((resolve, reject) => {
-  //     let value = DatabaseFunc.initDB(testPath);
-  //     resolve(value);
-  //   });
-  //   dbPromise.then((value: { content: any; error: any }) => {
-
-  //   });
-
+async function originalTests(testPath: string) {
   let result: any = await DatabaseFunc.initDB(testPath);
 
   console.log("Database init:");
   console.log(result);
-  
+
   console.log("-----");
   result = await DatabaseFunc.addAssignmentToDatabase(
     testPath,
@@ -24,7 +14,7 @@ export async function testDatabase() {
   );
   console.log("Add assignment:");
   console.log(result);
-  
+
   console.log("-----");
   result = await DatabaseFunc.getAssignmentFromDatabase(
     testPath,
@@ -48,7 +38,7 @@ export async function testDatabase() {
   );
   console.log("Get assignment:");
   console.log(result);
-  
+
   console.log("-----");
   result = await DatabaseFunc.deleteAssignmentFromDatabase(
     testPath,
@@ -56,7 +46,7 @@ export async function testDatabase() {
   );
   console.log("Delete assignment:");
   console.log(result);
-  
+
   console.log("-----");
   result = await DatabaseFunc.addModuleToDatabase(
     testPath,
@@ -93,4 +83,120 @@ export async function testDatabase() {
   result = await DatabaseFunc.deleteModule(testPath, testGlobals.testModule.ID);
   console.log("Delete module:");
   console.log(result);
+}
+
+async function assignmentTests(testPath: string) {
+  let result: any = await DatabaseFunc.initDB(testPath);
+
+  console.log("Database init:");
+  console.log(result);
+
+  console.log("-----");
+  result = await DatabaseFunc.addAssignmentToDatabase(
+    testPath,
+    testGlobals.testCurrentAssignment
+  );
+  console.log("Add assignment:");
+  console.log(result);
+
+  console.log("-----");
+  result = await DatabaseFunc.addAssignmentToDatabase(
+    testPath,
+    testGlobals.testCurrentAssignmentSecond
+  );
+  console.log("Add assignment:");
+  console.log(result);
+
+  console.log("-----");
+  result = await DatabaseFunc.getAllAssignments(testPath);
+  console.log("Get all assignments:");
+  console.log(result);
+
+  console.log("-----");
+  result = await DatabaseFunc.getAllAssignmentTags(testPath);
+  console.log("Get all assignment tags:");
+  console.log(result);
+
+  console.log("-----");
+  result = await DatabaseFunc.deleteAssignmentFromDatabase(
+    testPath,
+    testGlobals.testCurrentAssignment.assignmentID
+  );
+  console.log("Delete assignment:");
+  console.log(result);
+
+  console.log("-----");
+  result = await DatabaseFunc.deleteAssignmentFromDatabase(
+    testPath,
+    testGlobals.testCurrentAssignmentSecond.assignmentID
+  );
+  console.log("Delete assignment:");
+  console.log(result);
+}
+
+async function moduleTests(testPath: string) {
+  let result: any = await DatabaseFunc.initDB(testPath);
+
+  console.log("Database init:");
+  console.log(result);
+
+  console.log("-----");
+  result = await DatabaseFunc.addModuleToDatabase(
+    testPath,
+    testGlobals.testModule
+  );
+  console.log("Add module:");
+  console.log(result);
+
+  console.log("-----");
+  result = await DatabaseFunc.addModuleToDatabase(
+    testPath,
+    testGlobals.testModuleSecond
+  );
+  console.log("Add module:");
+  console.log(result);
+
+  console.log("-----");
+  result = await DatabaseFunc.getAllModules(testPath);
+  console.log("Get all modules:");
+  console.log(result);
+
+  console.log("-----");
+  result = await DatabaseFunc.getAllModuleTags(testPath);
+  console.log("Get all module tags:");
+  console.log(result);
+
+  console.log("-----");
+  result = await DatabaseFunc.getModuleCount(testPath);
+  console.log("Get module count:");
+  console.log(result);
+
+  console.log("-----");
+  result = await DatabaseFunc.deleteModule(testPath, testGlobals.testModule.ID);
+  console.log("Delete module:");
+  console.log(result);
+
+  console.log("-----");
+  result = await DatabaseFunc.deleteModule(
+    testPath,
+    testGlobals.testModuleSecond.ID
+  );
+  console.log("Delete module:");
+  console.log(result);
+}
+
+export async function testDatabase() {
+  //const testPath = "C:\\Users\\ramis\\Documents\\Procasma database test";
+  const testPath = "C:\\Users\\Elias\\Documents\\DEV\\Procasma\\";
+  //   const dbPromise = new Promise((resolve, reject) => {
+  //     let value = DatabaseFunc.initDB(testPath);
+  //     resolve(value);
+  //   });
+  //   dbPromise.then((value: { content: any; error: any }) => {
+
+  //   });
+
+  //originalTests(testPath);
+  //assignmentTests(testPath);
+  moduleTests(testPath);
 }
