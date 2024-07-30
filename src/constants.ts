6efe84ee3e9aa3c1ee208bc719991c0ab4797f15
Rami Saarivuori <167883444+RSaarivuoriLUT@users.noreply.github.com>
2024-07-30 11:13:21 +0300

    Correct texts in module add

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const version = "0.8.0";
+export const version = "0.8.1";
 export const courseMetaDataFileName = "course_info.json";
 export const DEVMODE = true;
 export const textExtensions = ["txt", "md", "csv", "log"];
