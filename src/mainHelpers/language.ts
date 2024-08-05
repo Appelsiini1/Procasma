60eb529c84fac42c0e8446bb73485dca856a3560
RSaarivuoriLUT <167883444+RSaarivuoriLUT@users.noreply.github.com>
2024-08-05 14:12:23 +0300

    Add language global to main

---
+++ src/mainHelpers/language.ts
@@ -0,0 +1,28 @@
+import { SupportedLanguages } from "../types";
+import texts from "../../resource/texts.json";
+
+class CurrentLanguage {
+  _language: SupportedLanguages;
+
+  set current(newLanguage: SupportedLanguages) {
+    this._language = newLanguage;
+  }
+  get current() {
+    return this._language;
+  }
+
+  constructor() {
+    this._language = "FI";
+  }
+}
+
+export const language = new CurrentLanguage();
+
+export function parseUICode(ui_code: string): string {
+  try {
+    const messageTranslated = (texts as any)?.[ui_code]?.[language.current];
+    return messageTranslated ?? ui_code;
+  } catch {
+    return ui_code;
+  }
+}
