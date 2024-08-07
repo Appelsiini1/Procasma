cb073d2a183a578a768027c33a547f33bee0e077
RSaarivuoriLUT <167883444+RSaarivuoriLUT@users.noreply.github.com>
2024-08-07 17:11:25 +0300

    Add method for set conversion

--- src/generalHelpers/assignment.ts
+++ src/generalHelpers/assignment.ts
@@ -1,8 +1,47 @@
-import { CodeAssignmentData } from "../types";
+import {
+  CodeAssignmentData,
+  CodeAssignmentSelectionData,
+  ExportSetData,
+  FullAssignmentSetData,
+} from "../types";
+import { coursePath } from "../globalsMain";
+import path from "path";
+import { assignmentDataFolder } from "src/constants";
+import { handleReadFileFS } from "src/mainHelpers/fileOperations";
 
 export function isExpanding(assignment: CodeAssignmentData) {
   if (assignment.next?.length > 0 || assignment.previous?.length > 0) {
     return true;
   }
   return false;
+}
+
+export function setToFullData(set: ExportSetData): FullAssignmentSetData {
+  let assignmentArray: CodeAssignmentSelectionData[] = [];
+  for (const setAssignment of set.assignments) {
+    const assigPath = path.join(
+      coursePath.path,
+      assignmentDataFolder,
+      setAssignment.folder,
+      setAssignment.id + ".json"
+    );
+    const fullData = handleReadFileFS(assigPath) as CodeAssignmentData;
+    let newAssignment: CodeAssignmentSelectionData = {
+      variation: fullData.variations[setAssignment.variationId],
+      CGid: setAssignment.CGid,
+      selectedPosition: setAssignment.selectedPosition,
+      selectedModule: setAssignment.selectedModule,
+      assignmentID: fullData.assignmentID,
+      level: fullData.level,
+      folder: fullData.folder,
+      codeLanguage: fullData.codeLanguage,
+      title: fullData.title,
+    };
+    assignmentArray.push(newAssignment);
+  }
+  const newSet: FullAssignmentSetData = {
+    assignmentArray: assignmentArray,
+    ...set,
+  };
+  return newSet;
 }
