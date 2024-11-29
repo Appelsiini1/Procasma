acc8c80815254dbb8189a3caf121494a4baf160e
RSaarivuoriLUT <167883444+RSaarivuoriLUT@users.noreply.github.com>
2024-11-29 11:40:45 +0200

    Add ATC component for test visuals

---
+++ src/components/CGAutoTestComponent.tsx
@@ -0,0 +1,54 @@
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionGroup,
+  AccordionSummary,
+  Typography,
+} from "@mui/joy";
+import { parseUICode } from "../rendererHelpers/translation";
+
+export default function CGAutoTestComponent(atvConfig: any) {
+  if (atvConfig === null) {
+    return <></>;
+  } else {
+    const setupPhase = JSON.stringify(
+      atvConfig["atvConfig"]["setup"]["steps"],
+      null,
+      2
+    );
+    const testPhase = JSON.stringify(
+      atvConfig["atvConfig"]["test"]["steps"],
+      null,
+      2
+    );
+
+    return (
+      <AccordionGroup size="sm" sx={{ width: "100%", marginRight: "2rem" }}>
+        <Accordion sx={{ backgroundColor: "#FaFaFa" }}>
+          <AccordionSummary sx={{ backgroundColor: "#D9D9D9" }}>
+            <Typography level="h4">{parseUICode("cg_setup")}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <pre>
+              <Typography sx={{ paddingLeft: "0.5em", lineHeight: "1.1" }}>
+                {setupPhase}
+              </Typography>
+            </pre>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion sx={{ backgroundColor: "#FaFaFa" }}>
+          <AccordionSummary sx={{ backgroundColor: "#D9D9D9" }}>
+            <Typography level="h4">{parseUICode("cg_tests")}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <pre>
+              <Typography sx={{ paddingLeft: "0.5em", lineHeight: "1.1" }}>
+                {testPhase}
+              </Typography>
+            </pre>
+          </AccordionDetails>
+        </Accordion>
+      </AccordionGroup>
+    );
+  }
+}
